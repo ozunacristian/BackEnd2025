4af1cb5e2cffe9d87b236774e355ebf8d4cb6498
ozunacristian <ozunacristianj@gmail.com>
2025-02-26 19:27:22 -0300

    Crear modelo de aplicación proyecto.

--- orion/apps/proyecto/models.py
+++ orion/apps/proyecto/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings # se importa settings para poder referenciar al modelo de usuario
 
-# Create your models here.
+class Proyecto(models.Model):
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+    # se relaciona el proyecto con el usuario que lo creó.
+    propietario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='proyectos_propietario')
+    # se relaciona el proyecto con los usuarios que participan en él.
+    participantes = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='proyectos_participantes')
+
+    def __str__(self):
+        return self.titulo
+
