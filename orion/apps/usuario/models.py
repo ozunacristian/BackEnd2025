73fd72116484e5559fdb433e6fd0d73a8ff6a1d6
ozunacristian <ozunacristianj@gmail.com>
2025-02-26 19:04:38 -0300

    Crear modelo de aplicación usuario.

--- orion/apps/usuario/models.py
+++ orion/apps/usuario/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    ADMINISTRADOR = 'administrador'
+    CREADOR = 'creador'
+    MIEMBRO = 'miembro'
+
+    ROLES_CHOICES = [
+        (ADMINISTRADOR, 'Administrador'),
+        (CREADOR, 'Creador'),
+        (MIEMBRO, 'Miembro'),
+    ]
+
+    rol = models.CharField(
+        max_length=20,
+        choices=ROLES_CHOICES,
+        default=MIEMBRO,
+    )
+
+    def __str__(self):
+        return self.username
