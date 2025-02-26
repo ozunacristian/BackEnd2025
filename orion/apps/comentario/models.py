3ce8bd01191954e93ef50260a03992b75eea3456
ozunacristian <ozunacristianj@gmail.com>
2025-02-26 20:50:04 -0300

    Crear modelo de aplicación comentario.

--- orion/apps/comentario/models.py
+++ orion/apps/comentario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Comentario(models.Model):
+    contenido = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    tarea = models.ForeignKey('tarea.Tarea', on_delete=models.CASCADE, related_name='comentario')
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comentario_autor')
+
+    def __str__(self):
+        return f'Comentario de {self.autor} en {self.tarea}'
