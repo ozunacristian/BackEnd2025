e1b5831b4f680522c60126f3238e22320ba25241
ozunacristian <ozunacristianj@gmail.com>
2025-02-26 20:10:27 -0300

    Crear modelo de aplicación tarea.

--- orion/apps/tarea/models.py
+++ orion/apps/tarea/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+from apps.proyecto.models import Proyecto
 
-# Create your models here.
+class Tarea(models.Model):
+    # Posibles estados de una tarea definidas en constantes para evitar errores de tipeo.
+    PENDIENTE = 'pendiente'
+    EN_PROGRESO = 'en_progreso'
+    COMPLETADA = 'completada'
+
+    ESTADO_CHOICES = [
+        (PENDIENTE, 'Pendiente'),
+        (EN_PROGRESO, 'En Progreso'),
+        (COMPLETADA, 'Completada'),
+    ]
+
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    fecha_inicio = models.DateTimeField(null=True, blank=True)
+    fecha_vencimiento = models.DateTimeField(null=True, blank=True)
+    estado = models.CharField(
+        max_length=20,
+        choices=ESTADO_CHOICES,
+        default=PENDIENTE,
+    )
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE, related_name='tareas')
+    responsable = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tareas_responsable')
+
+    def __str__(self):
+        return self.titulo
+
