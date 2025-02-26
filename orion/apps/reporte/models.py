8ffb73aad75d4388bca4bd2af081a3a6be331b12
ozunacristian <ozunacristianj@gmail.com>
2025-02-26 20:34:47 -0300

    Crear modelo de aplicación reporte.

--- orion/apps/reporte/models.py
+++ orion/apps/reporte/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Reporte(models.Model):
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    # se relaciona el reporte con el proyecto.
+    proyecto = models.ForeignKey('proyecto.Proyecto', on_delete=models.CASCADE, related_name='reportes')
+    # se relaciona el reporte con el usuario que lo creó.
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reportes_autor')
+
+    def __str__(self):
+        return self.titulo
+
