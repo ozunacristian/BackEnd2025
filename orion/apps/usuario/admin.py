224d3d428a802e58073ec716b1ed735a524b80b6
ozunacristian <ozunacristianj@gmail.com>
2025-02-26 23:52:49 -0300

    Registrar usuario en panel de admin.

--- orion/apps/usuario/admin.py
+++ orion/apps/usuario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.usuario.models import Usuario
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'rol', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('rol', 'is_staff', 'is_superuser', 'is_active')
