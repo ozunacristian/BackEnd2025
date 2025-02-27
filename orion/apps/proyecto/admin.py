5550a53724dfa4839f6367b5b5592391fbe47421
ozunacristian <ozunacristianj@gmail.com>
2025-02-27 00:08:54 -0300

    Registrar app proyecto en el panel de admin.

--- orion/apps/proyecto/admin.py
+++ orion/apps/proyecto/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.proyecto.models import Proyecto
 
-# Register your models here.
+@admin.register(Proyecto)
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha_creacion', 'fecha_actualizacion', 'propietario')
+    search_fields = ('titulo', 'descripcion', 'propietario__username')
+    list_filter = ('fecha_creacion', 'fecha_actualizacion', 'propietario')
