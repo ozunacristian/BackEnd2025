33ceef0bdd1795563ee972a0343f5a878143c0a1
ozunacristian <ozunacristianj@gmail.com>
2025-02-27 00:04:28 -0300

    Registrar aplicación reporte en el panel de admin.

--- orion/apps/reporte/admin.py
+++ orion/apps/reporte/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.reporte.models import Reporte
 
-# Register your models here.
+@admin.register(Reporte)
+class ReporteAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descripcion', 'fecha_creacion', 'proyecto', 'autor')
+    search_fields = ('titulo', 'descripcion', 'proyecto__titulo', 'autor__username')
+    list_filter = ('fecha_creacion', 'proyecto', 'autor')
