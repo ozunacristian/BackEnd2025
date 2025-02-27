35976e6b320c5654c9a3639f70962b0d510bff49
ozunacristian <ozunacristianj@gmail.com>
2025-02-27 00:01:49 -0300

    Registrar app tarea en el panel de admin.

--- orion/apps/tarea/admin.py
+++ orion/apps/tarea/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.tarea.models import Tarea
 
-# Register your models here.
+@admin.register(Tarea)
+class TareaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descripcion', 'fecha_inicio', 'fecha_vencimiento', 'estado', 'proyecto', 'responsable')
+    search_fields = ('titulo', 'descripcion', 'proyecto__titulo', 'responsable__username')
+    list_filter = ('estado', 'fecha_inicio', 'fecha_vencimiento', 'proyecto', 'responsable')
